9b4058d65ccf2aa76a80584e511afeacb14023bb
carloskauan <carloskauan190@gmail.com>
2022-04-26 17:14:34 -0300

    update api, add users, pets, routes, controllers, databases, configs

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres', 
     host: 'localhost',
     username: 'postgres',
-    password: 'postgres',
+    password: '',
     database: 'sqlnode',
     define: {
         timestamps: true,
