dd65fa6404d46395d843009ec8b64bd555d6e344
carloskauan <carloskauan190@gmail.com>
2022-04-27 12:42:51 -0300

    created migrations category, user_category

--- src/database/index.js
+++ src/database/index.js
@@ -4,14 +4,17 @@ const dbConfig = require('../config/database');
 // Imports Models
 const Pet = require('../models/Pet');
 const User = require('../models/User');
+const Category = require('../models/Category');
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Pet.init(connection);
+Category.init(connection);
 
 User.associate(connection.models);
 Pet.associate(connection.models);
+Category.associate(connection.models);
 
 module.exports = connection;
 
---
+++ src/database/migrations/20220427121436-create-user_categories.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.createTable('user_categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        alloNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'categories', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_categories');
+  }
+};
