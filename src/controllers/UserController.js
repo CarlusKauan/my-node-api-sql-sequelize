14e0fd20fbb1942d6d869e99c699879cb22a8d18
carloskauan <carloskauan190@gmail.com>
2022-04-28 15:12:51 -0300

    adicionando propriedades ao body store

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,9 +2,25 @@ const User = require('../models/User')
 
 module.exports = {
     async store(req, res) {
-        const { name, email } = req.body;
+        const {
+            name,
+            email,
+            senha,
+            telefone,
+            descricao,
+            data_nascimento,
+            rede_social
+        } = req.body;
 
-        const user = await User.create({ name, email });
+        const user = await User.create({
+            name,
+            email,
+            senha,
+            telefone,
+            descricao,
+            data_nascimento,
+            rede_social
+        });
 
         return res.json(user);
     },
