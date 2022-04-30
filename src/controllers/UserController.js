1a4235e2168cda12310f764e17141151273f8644
Sianguii <sisiestudos@gmail.com>
2022-04-30 01:28:08 -0300

    Adicionando metodo de encript (Falta testes)

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,32 +1,63 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
 module.exports = {
-    async store(req, res) {
-        const {
-            name,
-            email,
-            senha,
-            telefone,
-            descricao,
-            data_nascimento,
-            rede_social
-        } = req.body;
+  async store(req, res) {
+    const {
+      name,
+      email,
+      senha,
+      telefone,
+      descricao,
+      data_nascimento,
+      rede_social,
+    } = req.body;
 
-        const user = await User.create({
-            name,
-            email,
-            senha,
-            telefone,
-            descricao,
-            data_nascimento,
-            rede_social
-        });
+    const user = await User.create({
+      name,
+      email,
+      senha,
+      telefone,
+      descricao,
+      data_nascimento,
+      rede_social,
+    });
 
-        return res.json(user);
-    },
+    this.encript(senha); // chamando metodo de criptografia e inseirindo a Prop senha
+    return res.json(user);
+  },
 
-    async index(req, res) {
-        const users = await User.findAll()
-        return res.json(users);
-    },
+  async index(req, res) {
+    const users = await User.findAll();
+    return res.json(users);
+  },
+
+  //TODO METODO DE CRIPTOGRAFIA 
+  encript(password) {
+    const bcrypt = require("bcrypt");
+    const pass = password
+
+    for (let saltRounds = 10; saltRounds <= 15; saltRounds++) {
+      bcrypt.hash(pass, saltRounds).then((passHashed) => {
+        console.log(passHashed);
+      });
+    }
+  },
+
+
+
+  //TESTE AINDA
+ async Authenticate(user, senha){
+     const {nome} = user.body;
+     const pass = await User.findAll(nome = user)
+
+     const match = await bcrypt.compare(pass, senha)
+
+     if(match){
+        console.log("Permitido")
+     }
+     else{
+         console.log("Acesso negago")
+     }
+ }
+
 };
