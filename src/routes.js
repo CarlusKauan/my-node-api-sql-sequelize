58b193834d7c90b745aa52d7c495f42a7708ce04
Sianguii <sisiestudos@gmail.com>
2022-05-01 18:59:23 -0300

    Adicionando rota de autenticação/login

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const PetController  = require('./controllers/PetController');
 const CategoryController  = require('./controllers/CategoryController');
 
 const routes = express.Router()
+
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
 
@@ -14,5 +15,8 @@ routes.post('/users/:user_id/pets', PetController.store);
 routes.get('/users/:user_id/categories', CategoryController.index);
 routes.post('/users/:user_id/categories', CategoryController.store);
 routes.delete('/users/:user_id/categories', CategoryController.delete);
+routes.post('/login',UserController.login);
+
+
 
 module.exports = routes;
