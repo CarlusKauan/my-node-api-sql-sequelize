03bc3ed227f2e4e14da06685995118d4a579e325
Sianguii <sisiestudos@gmail.com>
2022-05-05 01:17:52 -0300

    Adicionando autorização de acesso na rota GET/users

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const express = require('express')
 const UserController = require('./controllers/UserController');
 const PetController  = require('./controllers/PetController');
 const CategoryController  = require('./controllers/CategoryController');
+const { verifyJwt } = require('./controllers/UserController');
+const { username } = require('./config/database');
 
 const routes = express.Router()
 
-routes.post('/users', UserController.store);
-routes.get('/users', UserController.index);
+routes.post('/users', UserController.store)
+routes.get('/users',verifyJwt ,UserController.index);
 
 routes.get('/users/pets', PetController.index);
 routes.get('/users/:user_id/pets', PetController.show);
