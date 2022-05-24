36723a5341639863757a5723b930619471f2595a
Sarah Conti <sarah.conti01@etec.sp.gov.br>
2022-05-24 01:00:31 -0300

    add conexão a solicitação

--- src/database/index.js
+++ src/database/index.js
@@ -5,16 +5,19 @@ const dbConfig = require('../config/database');
 const Pet = require('../models/Pet');
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Solicitacao = require('../models/Solicitacao');
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Pet.init(connection);
 Category.init(connection);
+Solicitacao.init(connection);
 
 User.associate(connection.models);
 Pet.associate(connection.models);
 Category.associate(connection.models);
+Solicitacao.associate(connection.models);
 
 module.exports = connection;
 
