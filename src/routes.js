dce078e7e38d7b5e072653d20947e91b06ff5722
Sarah Conti <sarah.conti01@etec.sp.gov.br>
2022-05-24 01:03:04 -0300

    add conexão a solicitação

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload');
 const UserController = require('./controllers/UserController');
 const PetController  = require('./controllers/PetController');
 const CategoryController  = require('./controllers/CategoryController');
+const SolicitacaoController  = require('./controllers/SolicitacaoController');
 const { verifyJwt } = require('./controllers/UserController');
 const { username } = require('./config/database');
 
@@ -20,11 +21,18 @@ routes.get('/users/pets', PetController.index); //buscar todos os pets
 routes.get('/users/:user_id/pets', PetController.show); // busca o pet do user
 routes.post('/users/:user_id/pets', upload.single('imagem'), PetController.store); //criando pet
 // routes.post('/users/:user_id/pets', upload.array('imagem', maxFotos), PetController.store);
+// routes.delete('/users/:user_id/pets', PetController.destroy);
 
 routes.get('/users/:user_id/categories', CategoryController.index);
 routes.post('/users/:user_id/categories', CategoryController.store);
 routes.delete('/users/:user_id/categories',verifyJwt, CategoryController.delete);
 routes.post('/login',UserController.login);
 
+
+//Rotas de solicitacao
+
+routes.post('/pets/:pets_id/solicitacoes', SolicitacaoController.store); //criando pet
+
+///pets/:pets_id/solicitacoes
 
 module.exports = routes;
