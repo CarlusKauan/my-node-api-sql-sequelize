db0aee23be0c9b9ee303e7d106c524fb0d3c5a6b
Sarah Conti <sarah.conti01@etec.sp.gov.br>
2022-06-07 17:57:25 -0300

    Atualizando migração de solicitação

---
+++ src/database/migrations/20220606040659-create-solicitacao.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+      await queryInterface.createTable('solicitacao', {
+        id: {
+          type:Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          alloNull: false
+        },
+        user_solicita: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: 'users', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        pets_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: 'pets', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        aprovado: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.dropTable('solicitacao');
+
+  }
+};
