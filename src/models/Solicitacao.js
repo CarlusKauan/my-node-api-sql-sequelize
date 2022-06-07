d60ded1b34f8f5f6212220aca8f7fc4ee8eec32c
Sarah Conti <sarah.conti01@etec.sp.gov.br>
2022-06-07 17:35:06 -0300

    add referencia da tabela em model

--- src/models/Solicitacao.js
+++ src/models/Solicitacao.js
@@ -3,15 +3,19 @@ const { Model, DataTypes } = require('sequelize')
 class Solicitacao extends Model{
   static init(sequelize) {
     super.init({
-      Aprovado: DataTypes.BOOLEAN
+      aprovado: DataTypes.BOOLEAN,
+
     }, {
-      sequelize
+      sequelize,
+      tableName: 'solicitacao'
+
     })
   }
 
     static associate(models) {
-      this.belongsTo(models.User, { foreignKey: 'user_id', as: 'owner' })
-      this.belongsTo(models.Pet, { foreignKey: 'pets_id ', as: 'solicitado' })
+      this.belongsTo(models.User, { foreignKey: 'user_solicita', as: 'solicita' });
+      this.belongsTo(models.Pet, { foreignKey: 'pets_id', as: 'solicitado'});
+
   }
 
 
