b0158e44c78e895544d8e5b5745bd03003d32e6c
Sarah Conti <sarah.conti01@etec.sp.gov.br>
2022-06-08 13:38:08 -0300

    Criando metodo store de reprovados

--- src/controllers/ReprovadoController.js
+++ src/controllers/ReprovadoController.js
@@ -1,5 +1,20 @@
-module.exports = {
-    store(req, res){
+const Solicitacao = require('../models/Solicitacao');
+const Pet = require('../models/Pet');
 
+
+module.exports = {
+    async store(req, res){
+      const{solicitacao_id} = req.params;
+      const solicitacao = await Solicitacao.findByPk(solicitacao_id,{
+        include: { association: 'solicitado'}
+      });
+
+      solicitacao.aprovado = false
+
+      await solicitacao.save()
+
+      return res.json(solicitacao);
     }
-}
+
+
+};
