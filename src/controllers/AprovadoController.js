eae69020e71102ada22315df8d52207400fd37df
Sarah Conti <sarah.conti01@etec.sp.gov.br>
2022-06-18 19:09:01 -0300

    Add metodo de busca de pets adotados

--- src/controllers/AprovadoController.js
+++ src/controllers/AprovadoController.js
@@ -1,22 +1,36 @@
 const Solicitacao = require('../models/Solicitacao');
 const Pet = require('../models/Pet');
-const { update } = require('../models/Pet');
+
 
 
 module.exports = {
     async store(req, res){
       const{solicitacao_id} = req.params;
+
+
       const solicitacao = await Solicitacao.findByPk(solicitacao_id,{
         include: { association: 'solicitado'}
+
       });
       solicitacao.aprovado = true
 
-      await solicitacao.save()
+
+      await solicitacao.save();
 
       return res.json(solicitacao);
 
     },
 
+
+
+
+    async GetAdotados(req, res){
+      const petAdotado = await Pet.findAll({
+          where : {situacao : 'Adotado'}
+      });
+      return res.json(petAdotado);
+
+    },
 
 
 };
