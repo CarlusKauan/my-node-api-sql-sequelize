fac6cf491c2883749aa3cc4525748bc355d0b40a
Sarah Conti <sarah.conti01@etec.sp.gov.br>
2022-06-26 03:20:38 -0300

    Alterando metodo de post  solicitacao

--- src/controllers/SolicitacaoController.js
+++ src/controllers/SolicitacaoController.js
@@ -3,29 +3,46 @@
 const Pet = require('../models/Pet');
 const Solicitacao = require('../models/Solicitacao');
 const User = require('../models/User');
-// const { store } = require('./PetController');
+
 
 module.exports = {
-
   async store(req, res) {
-    const { user_solicita, user_id } =  req.body;
+    const { filename } = req.file;
+    const { user_solicita, user_id} = req.headers;
     const { aprovado } = req.params;
     const { pets_id } = req.params;
+    const { name_solicita,telefone_solicita,name_pet,name_dono,
+      telefone_dono, imagem_solicita } = req.body;
+//,imagem_solicita
+
 
     console.log("pets_id", pets_id)
     console.log("user_solicita", user_solicita)
     console.log("aprovado", aprovado)
+    console.log("name_solicita", name_solicita)
+    console.log("name_dono", name_dono)
+    console.log("name_pet", name_pet)
+    console.log("telefone_solicita", telefone_solicita)
+    console.log("filename", filename)
 
 
-    const solicitacao = await Solicitacao.create({
+
+    const Recebersolicitacao = await Solicitacao.create({
       pets_id,
       user_solicita,
       user_id,
-      aprovado : false
+      name_solicita,
+      telefone_solicita,
+      imagem_solicita: filename,
+      aprovado : false,
+      name_pet,
+      name_dono,
+      telefone_dono,
+
 
     });
 
-    return res.json(solicitacao);
+    return res.json(Recebersolicitacao);
   },
 
 
@@ -33,7 +50,10 @@ module.exports = {
   //listar todas as solicitações
   async indexSoli(req, res) {
     const {user_id} = req.params;
-    const solicitacoes = await Solicitacao.findAll({where:{user_id}});
+    const solicitacoes = await Solicitacao.findAll({where:{user_id},
+      include: { association: 'solicitado' }
+    });
+
 
     if (!solicitacoes) {
       res.status(400).json({ error: 'Solicitation not found' });
@@ -44,23 +64,29 @@ module.exports = {
 
 
   //buscar solicitação por id
+
+
   async showSoli(req, res) {
     const { id } = req.params;
 
+
     console.log("id", id)
 
-    const solicitacao = await Solicitacao.findByPk(id,{
-      include: { association: 'solicitado' },
+    const solicitacao = await Solicitacao.findByPk(id,
+      { include: { association: 'owner' },
+
+    },
+    );
 
 
-    });
+    console.log("id", id)
+
+    // console.log("solicitado", solicitado)
 
     if (!solicitacao) {
       res.status(400).json({ error: 'Solicitation not found' });
+      // return res.status(200).json(solicitacao.include);
     }
-
-    return res.status(200).json(solicitacao.pets);
-
 
     return res.status(200).json(solicitacao);
   },
@@ -85,7 +111,8 @@ module.exports = {
       console.log("pets_id", pets_id)
 
       const pet = await Pet.findByPk(pets_id, {
-        include: { association: 'solicitado' }
+        include: { association: 'solicitado' },
+
       });
 
       return res.json(pet);
@@ -100,4 +127,5 @@ module.exports = {
 
       return res.status(200).send({ message: 'Solicitação Cancelada' });
     },
+
 };
